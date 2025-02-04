c0f8a1b17f0915303453392c5f9cf48cbfc01fc7
wafa <wafaaghraieb@gmail.com>
2025-02-04 15:08:50 +0100

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,13 @@ mod config;
 mod dtos;
 mod error;
 mod db;
-
+ use config::Config;
+use db::DBClient;
+#[derive(Debug,Clone)]
+pub struct AppState {
+    pub env:Config,
+    pub db_client: DBClient,
+}
+#[tokio::main]
 fn main() {
-    println!("Hello, world!");
 }
